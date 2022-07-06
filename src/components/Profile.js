d9a0b1d2b9e6e97355953eab89473f8676c4eb5d
isle88 <hyunjungkim2014@gmail.com>
2022-07-06 21:15:11 +0100

    updated component

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,26 +1,29 @@
 import React from "react";
+import { ArrowDown, Github, Linkedin } from "react-bootstrap-icons";
+import styles from "../styles/Profile.module.css";
 
 const Profile = () => {
   return (
-    <div>
-      <h1 style={{ fontStyle: "italic" }}>Profile</h1>
-      <h2>hello world ! 🙋‍♀️</h2>
-      <h3>I am Hyunjung Kim</h3>
-      <p>changed career to be a FrontEnd developer</p>
-      <p>Graduate Northcoders' Bootcamp on March 2022</p>
-      <p>i am blah blah</p>
-
-      <a href="https://github.com/isle88"  target="blank" >
-        <img
-          src="https://img.icons8.com/fluency/48/000000/github.png"
-          alt="github logo"
-        />
-      </a>
-      <a href="https://www.linkedin.com/in/hyunjung-kim-2843b3233/"  target="blank" >
-        <img
-          src="https://img.icons8.com/color/48/000000/linkedin-circled--v1.png"
-          alt="linkedin logo"
-        />
+    <div id="profile" className={`${styles.padding} ${styles.text}`}>
+      <h2 className={styles.title}>Hi! My name is Hyunjung Kim</h2>
+      <div className={styles.body}>
+        <p>I graduated Northcoders' Bootcamp in March 2022</p>
+        <p>I'm excited for you to see my portfolio!</p>
+        <p>Please check out also my Github and LinkedIn</p>
+      </div>
+      <div>
+        <a href="https://github.com/isle88" target="blank">
+          <Github size={48} className={styles.svg} />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/hyunjung-kim-2843b3233/"
+          target="blank"
+        >
+          <Linkedin color="black" size={48} className={styles.svg} />
+        </a>
+      </div>
+      <a href="#skills">
+        <ArrowDown color="grey" size={25} className={styles.arrow} />
       </a>
     </div>
   );
