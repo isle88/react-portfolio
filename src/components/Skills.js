f3d11e9fac6512dfb4b2a106e72a13ab8d598c58
isle88 <hyunjungkim2014@gmail.com>
2022-07-14 14:26:35 +0100

    improved css

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,56 +1,75 @@
 import React from "react";
 import skills from "../data/skills";
-import { ArrowDown } from "react-bootstrap-icons";
+import { ChevronDown } from "react-bootstrap-icons";
 import styles from "../styles/Skills.module.css";
 import Skill from "./Skill";
+import { Container, Row, Col } from "react-bootstrap";
 
 const Skills = () => {
   const { frontend, backend, deployment } = skills;
 
   return (
-    <div id="skills" className={`${styles.padding} ${styles.text}`}>
-      <div>
-        <h3 className={styles.title}>FrontEnd</h3>
-        {frontend.map((data) => {
-          return (
-            <Skill
-              image={data.image}
-              description={data.description}
-              name={data.name}
-              key={data.name}
-            />
-          );
-        })}
+    <div id="skills" className={styles.layout}>
+      <Container>
+        <Row>
+          <Col sm>
+            <div>
+              <h2 className={styles.title}>FrontEnd</h2>
+              <div className={styles.container}>
+                {frontend.map((data) => {
+                  return (
+                    <Skill
+                      image={data.image}
+                      description={data.description}
+                      name={data.name}
+                      key={data.name}
+                    />
+                  );
+                })}
+              </div>
+            </div>
+          </Col>
+          <Col sm>
+            <div>
+              <h2 className={styles.title}>BackEnd</h2>
+              <div className={styles.container}>
+                {backend.map((data) => {
+                  return (
+                    <Skill
+                      image={data.image}
+                      description={data.description}
+                      name={data.name}
+                      key={data.name}
+                    />
+                  );
+                })}
+              </div>
+            </div>
+          </Col>
+          <Col sm>
+            <div>
+              <h2 className={styles.title}>Deployment</h2>
+              <div className={styles.container}>
+                {deployment.map((data) => {
+                  return (
+                    <Skill
+                      image={data.image}
+                      description={data.description}
+                      name={data.name}
+                      key={data.name}
+                    />
+                  );
+                })}
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+      <div className={styles.down}>
+        <a href="#projects">
+          <ChevronDown color="grey" size={25} />
+        </a>
       </div>
-      <div>
-        <h3 className={styles.title}>BackEnd</h3>
-        {backend.map((data) => {
-          return (
-            <Skill
-              image={data.image}
-              description={data.description}
-              name={data.name}
-              key={data.name}
-            />
-          );
-        })}
-      </div>
-      <div>
-        <h3 className={styles.title}>Deployment</h3>
-        {deployment.map((data) => {
-          return (
-            <Skill
-              image={data.image}
-              description={data.description}
-              name={data.name}
-              key={data.name}
-            />
-          );
-        })}
-      </div>
-      <a href="#projects">
-        <ArrowDown color="grey" size={25} className={styles.arrow} />
-      </a>
     </div>
   );
 };
