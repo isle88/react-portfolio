5f3fa30545bad93f470b74323ff9eeca1a9e6f3e
isle88 <hyunjungkim2014@gmail.com>
2022-07-14 14:18:37 +0100

    changed file name

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import NavBar from "./components/NavBar";
 import Home from "./components/Home";
 import Profile from "./components/Profile";
 import Skills from "./components/Skills";
-import Projects from "./components/Project";
+import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 
 function App() {
