9f9eb0bf22a5c7e5aa4887e03b1447b4a4530243
isle88 <hyunjungkim2014@gmail.com>
2022-07-15 16:40:55 +0100

    changed data format

--- src/data/skills.js
+++ src/data/skills.js
@@ -1,61 +1,151 @@
-const skills = {
-  frontend: [
-    {
-      name: "JavaScript",
-      image: "https://img.icons8.com/color/48/000000/javascript--v1.png",
-      description: "javascript logo",
-    },
-    {
-      name: "HTML5",
-      image: "https://img.icons8.com/color/48/000000/html-5--v1.png",
-      description: "html5 logo",
-    },
-    {
-      name: "CSS3",
-      image: "https://img.icons8.com/color/48/000000/css3.png",
-      description: "css3 logo",
-    },
-    {
-      name: "React",
-      image: "https://img.icons8.com/color/48/000000/react-native.png",
-      description: "react logo",
-    },
-    {
-      name: "Bootstrap",
-      image: "https://img.icons8.com/color/48/000000/bootstrap.png",
-      description: "bootstrap logo",
-    },
-  ],
-  backend: [
-    {
-      name: "NodeJS",
-      image: "https://img.icons8.com/fluency/48/000000/node-js.png",
-      description: "nodejs logo",
-    },
-    {
-      name: "SQL",
-      image: "https://img.icons8.com/color/48/000000/sql.png",
-      description: "sql logo",
-    },
-    {
-      name: "MongoDB",
-      image: "https://img.icons8.com/color/48/000000/mongodb.png",
-      description: "mongodb logo",
-    },
-  ],
-  deployment: [
-    {
-      name: "Heroku",
-      image: "https://img.icons8.com/color/48/000000/heroku.png",
-      description: "heroku logo",
-    },
-    {
-      name: "Netlify",
-      image:
-        "https://img.icons8.com/external-tal-revivo-shadow-tal-revivo/48/000000/external-netlify-a-cloud-computing-company-that-offers-hosting-and-serverless-backend-services-for-static-websites-logo-shadow-tal-revivo.png",
-      description: "netlify logo",
-    },
-  ],
-};
+const skills = [
+  {
+    title: "Frontend",
+    skill: [
+      {
+        name: "JavaScript",
+        image: "https://img.icons8.com/color/48/000000/javascript--v1.png",
+        description: "javascript logo",
+      },
+      {
+        name: "HTML5",
+        image: "https://img.icons8.com/color/48/000000/html-5--v1.png",
+        description: "html5 logo",
+      },
+      {
+        name: "CSS3",
+        image: "https://img.icons8.com/color/48/000000/css3.png",
+        description: "css3 logo",
+      },
+      {
+        name: "React",
+        image: "https://img.icons8.com/color/48/000000/react-native.png",
+        description: "react logo",
+      },
+      {
+        name: "Bootstrap",
+        image: "https://img.icons8.com/color/48/000000/bootstrap.png",
+        description: "bootstrap logo",
+      },
+    ],
+  },
 
+  {
+    title: "Backend",
+    skill: [
+      {
+        name: "NodeJS",
+        image: "https://img.icons8.com/fluency/48/000000/node-js.png",
+        description: "nodejs logo",
+      },
+      {
+        name: "SQL",
+        image: "https://img.icons8.com/color/48/000000/sql.png",
+        description: "sql logo",
+      },
+      {
+        name: "MongoDB",
+        image: "https://img.icons8.com/color/48/000000/mongodb.png",
+        description: "mongodb logo",
+      },
+    ],
+  },
+
+  {
+    title: "Deployment",
+    skill: [
+      {
+        name: "Heroku",
+        image: "https://img.icons8.com/color/48/000000/heroku.png",
+        description: "heroku logo",
+      },
+      {
+        name: "Netlify",
+        image:
+          "https://img.icons8.com/external-tal-revivo-shadow-tal-revivo/48/000000/external-netlify-a-cloud-computing-company-that-offers-hosting-and-serverless-backend-services-for-static-websites-logo-shadow-tal-revivo.png",
+        description: "netlify logo",
+      },
+    ],
+  },
+
+  {
+    title: "Version Control",
+    skill: [
+      {
+        name: "Git",
+        image: "https://img.icons8.com/color/48/000000/git.png",
+        description: "git logo",
+      },
+    ],
+  },
+];
 export default skills;
+
+// const skills = {
+//   Frontend: [
+//     {
+//       name: "JavaScript",
+//       image: "https://img.icons8.com/color/48/000000/javascript--v1.png",
+//       description: "javascript logo",
+//     },
+//     {
+//       name: "HTML5",
+//       image: "https://img.icons8.com/color/48/000000/html-5--v1.png",
+//       description: "html5 logo",
+//     },
+//     {
+//       name: "CSS3",
+//       image: "https://img.icons8.com/color/48/000000/css3.png",
+//       description: "css3 logo",
+//     },
+//     {
+//       name: "React",
+//       image: "https://img.icons8.com/color/48/000000/react-native.png",
+//       description: "react logo",
+//     },
+//     {
+//       name: "Bootstrap",
+//       image: "https://img.icons8.com/color/48/000000/bootstrap.png",
+//       description: "bootstrap logo",
+//     },
+//   ],
+//   Backend: [
+//     {
+//       name: "NodeJS",
+//       image: "https://img.icons8.com/fluency/48/000000/node-js.png",
+//       description: "nodejs logo",
+//     },
+//     {
+//       name: "SQL",
+//       image: "https://img.icons8.com/color/48/000000/sql.png",
+//       description: "sql logo",
+//     },
+//     {
+//       name: "MongoDB",
+//       image: "https://img.icons8.com/color/48/000000/mongodb.png",
+//       description: "mongodb logo",
+//     },
+//   ],
+//   Deployment: [
+//     {
+//       name: "Heroku",
+//       image: "https://img.icons8.com/color/48/000000/heroku.png",
+//       description: "heroku logo",
+//     },
+//     {
+//       name: "Netlify",
+//       image:
+//         "https://img.icons8.com/external-tal-revivo-shadow-tal-revivo/48/000000/external-netlify-a-cloud-computing-company-that-offers-hosting-and-serverless-backend-services-for-static-websites-logo-shadow-tal-revivo.png",
+//       description: "netlify logo",
+//     },
+//   ],
+//   "Version Control": [
+//     {
+//       name: "Git",
+//       image: "https://img.icons8.com/color/48/000000/git.png",
+//       description: "git logo",
+//     },
+//   ],
+// };
+
+// export default skills;
