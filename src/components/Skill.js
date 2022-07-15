fc4ffc4a6e57d9a36309f5e0887ee6b6eff0aa68
isle88 <hyunjungkim2014@gmail.com>
2022-07-15 17:06:15 +0100

    updated component

--- src/components/Skill.js
+++ src/components/Skill.js
@@ -1,14 +1,39 @@
 import React from "react";
+import skills from "../data/skills";
+import PropTypes from "prop-types";
+import { Col } from "react-bootstrap";
+import Skills from "./Skills";
 
-const Skill = ({ image, description, name }) => {
+const Skill = ({ title, container }) => {
   return (
-    <div>
-      <span>
-        <img src={image} alt={description}></img>
-         {name} 
-      </span>
-    </div>
+    <>
+      {skills.map((category, index) => {
+        return (
+          <Col key={index}>
+            <div>
+              <h3 className={title}>{category.title}</h3>
+              <div className={container}>
+                {category.skill.map((data, index) => {
+                  return (
+                    <div key={index} className="align-middle">
+                      <img src={data.image} alt={data.description}></img>
+                      {data.name}
+                      <br />
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          </Col>
+        );
+      })}
+    </>
   );
+};
+
+Skill.propTypes = {
+  title: PropTypes.string.isRequired,
+  container: PropTypes.string.isRequired,
 };
 
 export default Skill;
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,68 +1,21 @@
 import React from "react";
-import skills from "../data/skills";
 import { ChevronDown } from "react-bootstrap-icons";
+import { Container, Row } from "react-bootstrap";
 import styles from "../styles/Skills.module.css";
 import Skill from "./Skill";
-import { Container, Row, Col } from "react-bootstrap";
 
 const Skills = () => {
-  const { frontend, backend, deployment } = skills;
-
   return (
     <div id="skills" className={styles.layout}>
-      <Container>
-        <Row>
-          <Col lg>
-            <div>
-              <h2 className={styles.title}>FrontEnd</h2>
-              <div className={styles.container}>
-                {frontend.map((data) => {
-                  return (
-                    <Skill
-                      image={data.image}
-                      description={data.description}
-                      name={data.name}
-                      key={data.name}
-                    />
-                  );
-                })}
-              </div>
-            </div>
-          </Col>
-          <Col sm>
-            <div>
-              <h2 className={styles.title}>BackEnd</h2>
-              <div className={styles.container}>
-                {backend.map((data) => {
-                  return (
-                    <Skill
-                      image={data.image}
-                      description={data.description}
-                      name={data.name}
-                      key={data.name}
-                    />
-                  );
-                })}
-              </div>
-            </div>
-          </Col>
-          <Col sm>
-            <div>
-              <h2 className={styles.title}>Deployment</h2>
-              <div className={styles.container}>
-                {deployment.map((data) => {
-                  return (
-                    <Skill
-                      image={data.image}
-                      description={data.description}
-                      name={data.name}
-                      key={data.name}
-                    />
-                  );
-                })}
-              </div>
-            </div>
-          </Col>
+      <Container >
+        <Row lg={4} md={2} xs={1} >
+          <>
+            <Skill
+              title={styles.title}
+              container={styles.container}
+              border={styles.border}
+            />
+          </>
         </Row>
       </Container>
       <div className={styles.down}>
